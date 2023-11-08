2a59690430d5734f9776a17843e55624e7354363
hieunmh <hieunm.pt@gmail.com>
2023-11-08 20:32:12 +0700

    menu UI

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -4,26 +4,109 @@ import EventScreen from '../screens/EventScreen';
 import Attraction from '../screens/Attraction';
 import Map from '../screens/Map';
 import Service from '../screens/Service';
-import { Text, View } from 'react-native';
+
+import { Entypo } from '@expo/vector-icons'; 
+import { FontAwesome5 } from '@expo/vector-icons'; 
+import { AntDesign } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
   return (
-    <Tab.Navigator className='bg-[#ffffff] rounded-[15] h-[90px]'>
-      <Tab.Screen name='Di tích' component={Attraction} 
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: "#fca906",
+        tabBarActiveBackgroundColor: "#fff",
+        tabBarInactiveBackgroundColor: "#fff",
+        tabBarStyle: {
+          borderWidth: 0,
+          borderRadius: 0,
+          height: 60,
+          paddingBottom: 5,
+        },
+      }}
+    >
+      <Tab.Screen name='Trang chủ' component={HomeScreen} 
         options={{
-          tabBarIcon: ({focused}) => {
-            <View>
-              <Text>Di tích</Text>
-            </View>
+          tabBarLabel: 'Trang chủ',
+          headerShown: true,
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Entypo name="home" size={24} color="#fca906" />
+            ) : (
+              <Entypo name="home" size={24} color="#c0c8ca" />
+            ),
+          tabBarLabelStyle: {
+            fontSize: 12
           }
         }}
       />
-      <Tab.Screen name='Bản đồ' component={Map} />
-      <Tab.Screen name='Trang chủ' component={HomeScreen} />
-      <Tab.Screen name='Sự kiện' component={EventScreen} />
-      <Tab.Screen name='Dịch vụ' component={Service} />
+
+      <Tab.Screen name='Di tích' component={Attraction} 
+        options={{
+          tabBarLabel: 'Di tích',
+          headerShown: false,
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <MaterialCommunityIcons name="castle" size={24} color="#fca906" />
+            ) : (
+              <MaterialCommunityIcons name="castle" size={24} color="#c0c8ca" />
+            ),
+          tabBarLabelStyle: {
+            fontSize: 12
+          }
+        }}
+      />
+
+      <Tab.Screen name='Bản đồ' component={Map} 
+        options={{
+          tabBarLabel: 'Bản đồ',
+          headerShown: false,
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <FontAwesome5 name="map-marker-alt" size={24} color="#fca906" />
+            ) : (
+              <FontAwesome5 name="map-marker-alt" size={24} color="#c0c8ca" />
+            ),
+          tabBarLabelStyle: {
+            fontSize: 12
+          }
+        }}
+      />
+  
+      <Tab.Screen name='Sự kiện' component={EventScreen} 
+        options={{
+          tabBarLabel: 'Sự kiện',
+          headerShown: false,
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <FontAwesome5 name="calendar-day" size={24} color="#fca906" />
+            ) : (
+              <FontAwesome5 name="calendar-day" size={24} color="#c0c8ca" />
+            ),
+          tabBarLabelStyle: {
+            fontSize: 12
+          }
+        }}
+      />
+
+      <Tab.Screen name='Dịch vụ' component={Service} 
+        options={{
+          tabBarLabel: 'Dịch vụ',
+          headerShown: false,
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <AntDesign name="appstore1" size={24} color="#fca906" />
+            ) : (
+              <AntDesign name="appstore1" size={24} color="#c0c8ca" />
+            ),
+          tabBarLabelStyle: {
+            fontSize: 12
+          }
+        }}
+        
+      />
     </Tab.Navigator>
   )
 }
