602eff889458fcb91d01c4af90f2ab4c0115799c
ctpanh <ctpanh03@gmail.com>
2023-12-02 20:42:09 +0700

    fix: list of attraction

--- screens/Attraction.tsx
+++ screens/Attraction.tsx
@@ -14,6 +14,7 @@ import filterVN from "../filterVN";
 import { attractions } from "../data/index.js";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import AttractionDetail from "./AttractionDetail";
 
 export type RootStackParamList = {
   AttractionDetail:
@@ -29,26 +30,6 @@ export type RootStackParamList = {
 };
 const screenWidth = Dimensions.get("window").width;
 
-// const attractions = [
-//   {
-//     id: 1,
-//     name: 'Đền Bái Đính',
-//     description: 'Đền Bái Đính là một ngôi đền lớn tại huyện Gia Viễn, tỉnh Ninh Bình, Việt Nam. Đền Bái Đính nằm ở núi Bái Sơn, cách trung tâm thành phố Ninh Bình khoảng 15 km về phía tây nam. Đền Bái Đính được xây dựng từ thế kỷ 11, thời kỳ Lý, nhưng đã bị thiêu hủy vào thời kỳ nhà Trần. Năm 2003, ngôi đền được xây dựng lại, và đến năm 2010, ngôi đền đã hoàn thành.',
-//     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Bai_Dinh_Pagoda_2019.jpg/800px-Bai_Dinh_Pagoda_2019.jpg'
-//   },
-//   {
-//     id: 2,
-//     name: 'Hang Múa',
-//     description: 'Hang Múa là một hang động nằm ở xã Ninh Xuân, huyện Hoa Lư, tỉnh Ninh Bình, cách trung tâm thành phố Ninh Bình khoảng 7 km về phía nam. Hang Múa có diện tích khoảng 800 m2, nằm ở độ cao 800 m so với mực nước biển, trên một ngọn núi có tên là Múa.',
-//     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Hang_Mua_2019.jpg/800px-Hang_Mua_2019.jpg'
-//   },
-//   {
-//     id: 3,
-//     name: 'Tràng An',
-//     description: 'Tràng An là một di sản văn hóa và thiên nhiên thế giới tại Việt Nam, nằm ở xã Ninh Hải, huyện Hoa Lư, tỉnh Ninh Bình, cách trung tâm thành phố Ninh Bình khoảng 7 km về phía nam. Di sản này được UNESCO công nhận vào ngày 23 tháng 6 năm 2014, trong kỳ họp thứ 38 của Ủy ban Di sản thế giới.',
-//   },
-// ];
-
 const AttractionScreen = () => {
   let [searchIp, setSearchIp] = useState("");
   let [active, setActive] = useState(0);
@@ -56,13 +37,15 @@ const AttractionScreen = () => {
   const intervalRef = useRef<NodeJS.Timeout | null>(null);
   const navigation =
     useNavigation<NativeStackNavigationProp<RootStackParamList>>();
-  const [showOtherComponent, setShowOtherComponent] = useState(false);
+  const [showMainComponent, setShowMainComponent] = useState(false);
+  const [showAttractionDetail, setShowAttractionDetail] = useState(false);
+  const [idAttraction, setIdAttraction] = useState<number | null>(null);
   useEffect(() => {
     navigation.addListener("focus", () => {
-      setShowOtherComponent(false);
+      setShowMainComponent(false);
       setSearchIp("");
     });
-  }, [showOtherComponent]);
+  }, [showMainComponent]);
   useEffect(() => {
     if (step.current != null) {
       if (attractions.length > 0) {
@@ -105,100 +88,115 @@ const AttractionScreen = () => {
 
   return (
     <SafeAreaView className="flex-1 bg-white relative pt-8">
-      {showOtherComponent ? (
-        <>
-          <View className="w-full h-1/3">
-            <ScrollView
-              onScroll={({ nativeEvent }) => onchange(nativeEvent)}
-              scrollEventThrottle={16}
-              showsHorizontalScrollIndicator={false}
-              pagingEnabled
-              horizontal
-              className="flex w-full h-full"
-              ref={step}
-            >
-              {attractions.map((attraction) => (
-                <View key={attraction.id}>
-                  <Image
-                    source={attraction.image}
-                    className="w-screen h-full"
-                  />
-                </View>
-              ))}
-            </ScrollView>
-
-            <View className="absolute flex-row bottom-[10%] self-center">
-              {attractions.map((attraction, index) => (
-                <Text
-                  key={index}
-                  className={`mx-2 text-xl ${
-                    index == active ? "text-orange-300" : "text-white"
-                  }`}
-                >
-                  ⬤
-                </Text>
-              ))}
-            </View>
-          </View>
-
-          <View className="flex flex-col items-center justify-between">
-            <View className="mx-6 flex flex-row bg-gray-100 -translate-y-4 items-center rounded-full px-5 shadow-md">
-              <FontAwesome
-                name="search"
-                size={20}
-                color="#b1b1b1"
-                className=""
-              />
-              <TextInput
-                defaultValue={searchIp}
-                onChangeText={(text) => setSearchIp(text)}
-                placeholder="Tìm kiếm..."
-                className="h-12 w-full text-lg pl-4"
-              />
-            </View>
-
-            <View>
-              <Text className="text-[#DC812D] text-xl font-bold">
-                Các Di tích tại Ninh Bình
-              </Text>
-            </View>
-          </View>
-
-          <ScrollView className="mt-4 mb-16">
-            {attractions.map((attraction) => (
-              <TouchableOpacity
-                key={attraction.id}
-                onPress={() =>
-                  navigation.navigate("AttractionDetail", { attraction })
-                }
+      {showMainComponent ? (
+        showAttractionDetail ? (
+          <AttractionDetail
+            attraction={attractions[idAttraction!]}
+            setShowAttractionDetail={setShowAttractionDetail}
+          />
+        ) : (
+          <>
+            <View className="w-full h-1/3">
+              <ScrollView
+                onScroll={({ nativeEvent }) => onchange(nativeEvent)}
+                scrollEventThrottle={16}
+                showsHorizontalScrollIndicator={false}
+                pagingEnabled
+                horizontal
+                className="flex w-full h-full"
+                ref={step}
               >
-                {filterVN(attraction.name.toLowerCase()).includes(
-                  searchIp.toLowerCase()
-                ) && (
-                  <View
-                    className={`flex flex-row px-4 py-4 items-center justify-between ${
-                      attraction.id % 2 == 0 ? "bg-[#f1f1f1]" : ""
-                    }`}
-                  >
+                {attractions.map((attraction) => (
+                  <View key={attraction.id}>
                     <Image
                       source={attraction.image}
-                      className="flex w-[150px] h-[100px] rounded-xl"
+                      className="w-screen h-full"
                     />
-
-                    <View className="flex-1 items-center text-center">
-                      <Text className="flex font-bold text-center text-xl">
-                        {attraction.name}
-                      </Text>
-                      <Text className="text-gray-500">
-                        {attraction.description}
-                      </Text>
-                    </View>
                   </View>
-                )}
-              </TouchableOpacity>
-            ))}
-          </ScrollView>
-        </>
+                ))}
+              </ScrollView>
+
+              <View className="absolute flex-row bottom-[10%] self-center">
+                {attractions.map((attraction, index) => (
+                  <Text
+                    key={index}
+                    className={`mx-2 text-xl ${
+                      index == active ? "text-orange-300" : "text-white"
+                    }`}
+                  >
+                    ⬤
+                  </Text>
+                ))}
+              </View>
+            </View>
+
+            <View className="flex flex-col items-center justify-between">
+              <View className="mx-6 flex flex-row bg-gray-100 -translate-y-4 items-center rounded-full px-5 shadow-md">
+                <FontAwesome
+                  name="search"
+                  size={20}
+                  color="#b1b1b1"
+                  className=""
+                />
+                <TextInput
+                  defaultValue={searchIp}
+                  onChangeText={(text) => setSearchIp(text)}
+                  placeholder="Tìm kiếm..."
+                  className="h-12 w-full text-lg pl-4"
+                />
+              </View>
+
+              <View>
+                <Text className="text-[#DC812D] text-xl font-bold">
+                  Các Di tích tại Ninh Bình
+                </Text>
+              </View>
+            </View>
+            <ScrollView className="mt-4 mb-16">
+              <View className="gap-4">
+                {attractions.map((attraction, index) => (
+                  <View
+                    key={index}
+                    className={`h-fit flex flex-col justify-center items-center px-4 py-2 gap-4 ${
+                      index % 2 == 0 ? "" : "bg-[#f1f1f1]"
+                    }`}
+                  >
+                    {filterVN(attraction.name.toLowerCase()).includes(
+                      searchIp.toLowerCase()
+                    ) && (
+                      <>
+                        <View className="flex flex-col justify-center items-center">
+                          <Image
+                            source={attraction.image}
+                            className={`rounded w-[181px] h-[181px]`}
+                          />
+                        </View>
+                        <View className="gap text-xs font-bold tracking-tight text-white opacity-50">
+                          <Text className="flex text-center text-xl">
+                            {attraction.name}
+                          </Text>
+                          <Text>
+                            {attraction.description.slice(0, 100) + "..."}
+                          </Text>
+                          <TouchableOpacity
+                            onPress={() => {
+                              setIdAttraction(index);
+                              setShowAttractionDetail(true);
+                            }}
+                          >
+                            <Text className="flex text-center text-[#DC812D] font-bold">
+                              Xem thêm
+                            </Text>
+                          </TouchableOpacity>
+                        </View>
+                      </>
+                    )}
+                  </View>
+                ))}
+              </View>
+            </ScrollView>
+          </>
+        )
       ) : (
         <View className="w-full ">
           <Image
@@ -211,7 +209,7 @@ const AttractionScreen = () => {
             </Text>
             <TouchableOpacity
               className="w-1/2 flex items-center rounded bg-[#fca906]"
-              onPress={() => setShowOtherComponent(true)}
+              onPress={() => setShowMainComponent(true)}
             >
               <Text className="text-white text-3xl font-bold">Bắt đầu</Text>
             </TouchableOpacity>
--- screens/AttractionDetail.tsx
+++ screens/AttractionDetail.tsx
@@ -3,41 +3,40 @@ import { View, Text, Image, Button } from "react-native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RouteProp } from "@react-navigation/native";
 
-type RootStackParamList = {
-    AttractionDetail: { attraction: { image: any; name: string; description: string } };
-};
-
-type AttractionDetailScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, "AttractionDetail">;
-type AttractionDetailScreenRouteProp = RouteProp<RootStackParamList, "AttractionDetail">;
-
 interface AttractionDetailProps {
-    navigation: AttractionDetailScreenNavigationProp;
-    route: AttractionDetailScreenRouteProp;
+  attraction: {
+    id: number;
+    name: string;
+    description: string;
+    image: any;
+  };
+  setShowAttractionDetail: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const AttractionDetail: React.FC<AttractionDetailProps> = ({ navigation, route }) => {
-    const { attraction } = route.params;
+const AttractionDetail: React.FC<AttractionDetailProps> = ({
+  attraction,
+  setShowAttractionDetail,
+}) => {
 
-    return (
-        <View>
-            <Image source={attraction.image} style={{ width: 200, height: 200 }} />
-            <View>
-                <Text>{attraction.name}</Text>
-            </View>
-            <View>
-                <Text>{attraction.description}</Text>
-            </View>
-            <View>
-                <Button
-                    title="Back"
-                    onPress={() => {
-                        navigation.goBack();
-                    }}
-                />
-            </View>
-        </View>
-    );
+  return (
+    <View>
+      <Image source={attraction.image} style={{ width: 200, height: 200 }} />
+      <View>
+        <Text>{attraction.name}</Text>
+      </View>
+      <View>
+        <Text>{attraction.description}</Text>
+      </View>
+      <View>
+        <Button
+          title="Back"
+          onPress={() => {
+            setShowAttractionDetail(false);
+          }}
+        />
+      </View>
+    </View>
+  );
 };
 
 export default AttractionDetail;
-
