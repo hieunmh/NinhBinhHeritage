ff004344f3ee3b628c406c337e7b29412a27c215
TORIOP23 <vinhtran23022003@gmail.com>
2023-12-06 17:58:37 +0700

    draft UI-service

--- screens/Service.tsx
+++ screens/Service.tsx
@@ -1,17 +1,72 @@
-import { SafeAreaView } from "react-native";
-import { Text, View } from 'react-native';
+import { Text, View, SafeAreaView, StyleSheet } from 'react-native';
+import { Fontisto } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
 
 const ServiceScreen = () => {
   return (
     <SafeAreaView>
-      <View className="flex-row items-center justify-between px-8">
-        <View>
-          <Text className="text-4xl text-[#0B646B] font-bold">Dich Vu</Text>
-          <Text className="text-[#527283] text-3xl">Service screen</Text>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Dịch vụ</Text>
+      </View>
+      <View style={styles.container}>
+        <View style={styles.item}>
+          <Fontisto name="ticket" size={24} color="black" />
+          <Text>Vé điện tử</Text>
+        </View>
+        <View style={styles.item}>
+          <MaterialIcons name="electric-car" size={24} color="black" />
+          <Text>Xe điện</Text>
+        </View>
+        <View style={styles.item}>
+          <MaterialCommunityIcons name="food-turkey" size={24} color="black" />
+          <Text>Đồ ăn</Text>
+        </View>
+        <View style={styles.item}>
+          <FontAwesome name="headphones" size={24} color="black" />
+          <Text>Hướng dẫn Audio</Text>
+        </View>
+        <View style={styles.item}>
+          <MaterialCommunityIcons name="transit-detour" size={24} color="black" />
+          <Text>Tour</Text>
+        </View>
+        <View style={styles.item}>
+          <Entypo name="camera" size={24} color="black" />
+          <Text>Chụp ảnh</Text>
         </View>
       </View>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  header: {
+    height: 60,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#33EEFF'
+
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    flexWrap: 'wrap',
+  },
+  item: {
+    height: 100,
+    flexBasis: '33.333%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
 
 export default ServiceScreen;
